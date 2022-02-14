479b9cbbb8e2acd7b3fa330f545498d6e0603390
João Xavier <jvxavier.lima67@gmail.com>
2022-02-14 13:10:45 -0300

    Add files via upload

---
+++ ExercicioEscada/index.js
@@ -0,0 +1,12 @@
+let tamEscada = 6 // Aqui definimos o tamanho da escada
+
+for (let i = 0; i < tamEscada; i++) {
+    for (let j = 0; j < tamEscada; j++) {
+        if(j > (tamEscada-2-i)){
+            document.write("*")
+        }else{
+            document.write("&nbsp&nbsp")
+        }
+    }   
+    document.write("<br/>"); 
+}
