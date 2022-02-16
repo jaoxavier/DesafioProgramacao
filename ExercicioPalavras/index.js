fa3646de1472d11a2943b49c657b6f4df39c5daf
João Xavier <jvxavier.lima67@gmail.com>
2022-02-15 22:50:41 -0300

    Add files via upload

--- ExercicioPalavras/index.js
+++ ExercicioPalavras/index.js
@@ -1,8 +1,15 @@
 let word = prompt("Qual a palavra?");
-let repeatedWords = [];
+let repeatedWords = new Array();
 let letterIndex = [];
 let cont = 0;
 
+function filterArray(unfilteredArray){
+    var repeated = {};
+    var filteredArray = unfilteredArray.filter(function(item){
+        return repeated.hasOwnProperty(item)? false : (repeated[item]=true);
+    });
+    return filteredArray;
+}
 
 for (let i = 0; i < word.length; i++) {
     for (let j = i+1; j < word.length; j++) {
@@ -27,5 +34,7 @@ for (let i = 0; i < letterIndex.length; i++) {
     }
 }
 
-console.log(letterIndex);
-console.log(repeatedWords);
+let filteredArray = filterArray(repeatedWords)
+console.log(filteredArray);
+
+document.write("<h1>" + filteredArray.length + "</h1>")
